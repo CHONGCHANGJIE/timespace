1598a5e34233c1cfe15d6f3645c8a7266a9d43e6
CHONGCHANGJIE <jie_chong@hotmail.com>
2017-04-20 14:41:48 +0800

    AddAuthenticationGmail

--- scheduler/src/app/app.module.ts
+++ scheduler/src/app/app.module.ts
@@ -13,9 +13,10 @@ import { JobsComponent } from './jobs/jobs.component';
 import { InventoryComponent } from './inventory/inventory.component';
 import 'rxjs/add/operator/map';
 import { InventoryFormComponent } from './inventory/inventory-form/inventory-form.component';
-import { AngularFireModule } from 'angularfire2';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 import { InventoryService } from './inventory/inventory.service';
 import { FilterPipe } from './filter.pipe'
+import {FlashMessagesModule} from 'angular2-flash-messages';
 
 
 export const firebaseConfig = {
@@ -27,6 +28,11 @@ export const firebaseConfig = {
  storageBucket: "timespace-45075.appspot.com",
  messagingSenderId: "15827274381"
 };
+
+const firebaseAuthConfig= {
+ provider: AuthProviders.Google,
+ method: AuthMethods.Popup
+}
 
 @NgModule({
   declarations: [
@@ -47,8 +53,8 @@ export const firebaseConfig = {
     HttpModule,
     RouterModule.forRoot([]),
     AppRoutes,
-    AngularFireModule.initializeApp(firebaseConfig)
-
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig),
+    FlashMessagesModule
   ],
   providers: [InventoryService],
   bootstrap: [AppComponent]
--- scheduler/src/app/nav/nav.component.ts
+++ scheduler/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import {AngularFire} from 'angularfire2';
+import {Router} from '@angular/router';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-nav',
@@ -10,13 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(public af: AngularFire, private router:Router, public flashMessage: FlashMessagesService ) { }
 
   ngOnInit() {
   }
 
    //collapse content
 
+   login(){
+    this.af.auth.login();
+    this.router.navigate(['']);
+   }
 
-
+   logout(){
+    this.af.auth.logout();
+    this.router.navigate(['']);
+    this.flashMessage.show('You are logged out',{cssClass:'alert-success',timeout : 4000})
+   }
 }
